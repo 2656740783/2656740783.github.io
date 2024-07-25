a31b069bedd689d39894d23839a403e600d7ead3
2656740783 <2656740783@qq.com>
2024-07-25 15:31:03 +0800

    Site updated: 2024-07-25 15:30:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/08/hello-world/","2024/06/08/newpapername/"];function toRandomPost(){
+var posts=["2024/07/20/hello-world/","2024/06/08/newpapername/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
